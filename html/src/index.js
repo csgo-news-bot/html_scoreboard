2521ccf4cc44a7a7a4c0c0514783f2b382f11857
Romuald Shmidtelson <d-w-b@mail.ru>
2021-01-04 01:22:38 +0300

    Added templat

--- html/src/index.js
+++ html/src/index.js
@@ -1,14 +1,15 @@
 import './assets/css/index.module.scss';
 
 const root = document.getElementById('root');
+const urlParams = new URLSearchParams(window.location.search);
 
-root.innerHTML =
-  '<h1>Результаты матча</h1>\n' +
-  '<h2>%команда1% vs %команда2%</h2>\n' +
-  '<h2>Турнир: %Название турнира%</h2>\n' +
-  '<h2>Счет: %счет%</h2>';
+root.innerHTML = `
+<h1>Результаты матча</h1>
+<h2>${ urlParams.get("team_won_name") } vs ${ urlParams.get("team_lose_name") }</h2>
+<h2>Турнир: ${ urlParams.get("championship_name") }</h2>
+<h2>Счет: ${ urlParams.get("team_won_score") } - ${ urlParams.get("team_lose_score") } (${ urlParams.get("match_type") })</h2>
+`;
 
-// This is needed for Hot Module Replacement
 if (module.hot) {
   module.hot.accept();
 }
