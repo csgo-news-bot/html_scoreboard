bea8c0ccda11c8379d82a36bef42e8809c710853
Romuald Shmidtelson <d-w-b@mail.ru>
2021-01-10 10:07:23 +0300

    Updated html markup

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,16 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 import styles from './assets/css/index.module.scss';
 
-// const urlParams = new URLSearchParams(window.location.search);
-// const championshipName = urlParams.get('championship_name');
-// const teamWonName = urlParams.get('team_won_name');
-// const teamLoseName = urlParams.get('team_lose_name');
-// const teamWonScore = urlParams.get('team_won_score');
-// const teamLoseScore = urlParams.get('team_lose_score');
-// const matchType = urlParams.get('match_type');
+const urlParams = new URLSearchParams(window.location.search);
+const championshipName = urlParams.get('championship_name');
+const teamWonName = urlParams.get('team_won_name');
+const teamLoseName = urlParams.get('team_lose_name');
+const teamWonScore = urlParams.get('team_won_score');
+const teamLoseScore = urlParams.get('team_lose_score');
+const matchType = urlParams.get('match_type');
+const teamWonLogoUrl = urlParams.get('team_won_logo_url');
+const teamLoseLogoUrl = urlParams.get('team_lose_logo_url');
 
-const championshipName = 'BLAST Premier: Global Final 2020';
-const teamWonName = 'Natus Vincere';
-const teamLoseName = 'Virtus PRO';
-const teamWonScore = 16;
-const teamLoseScore = 14;
-const matchType = 'bo1';
-const eventLogoUrl = 'https://storage.googleapis.com/csgo_global_elite/team/natus_vincere.png';
-const teamWonLogoUrl = 'https://storage.googleapis.com/csgo_global_elite/team/natus_vincere.png';
-const teamLoseLogoUrl = 'https://storage.googleapis.com/csgo_global_elite/team/natus_vincere.png';
-
-const eventLogoStyle = {
-  backgroundImage: `url(${eventLogoUrl})`,
-};
 const teamWonLogoStyle = {
   backgroundImage: `url(${teamWonLogoUrl})`,
 };
@@ -33,7 +22,6 @@ const teamLoseLogoStyle = {
 const App = () => (
   <div className={styles.container}>
     <div className={styles.top}>
-      <div className={styles.event_logo} style={eventLogoStyle} />
       <div className={styles.event_name}>
         {championshipName.toUpperCase()}
       </div>
